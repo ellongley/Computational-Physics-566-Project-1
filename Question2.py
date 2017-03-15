e87eadc546b168ee067fdb6a4295d276671e4307
DebrajGhose <dg.chaos06@gmail.com>
2017-03-15 19:35:45 -0400

    Captured distribution at five different time points
    Plotted results in one single graph

--- Question2.py
+++ Question2.py
@@ -9,7 +9,7 @@ from pylab import *
 
 
 D = 2 #Diffusion constant
-dt = 0.0001 #time discretization
+dt = 0.0005 #time discretization
 L = 10 #length of domain
 
 N = 100 #number of discrete points
@@ -24,25 +24,49 @@ conc1 = np.zeros(N) #conccentration at next time step
 #setting up intial conditions
 
 conc[N/2-5:N/2+5] = 10 #concentration of chemical 
-plt.figure(1)
-plt.plot(x,conc)
-
+c0 = np.copy(conc[:]) # store initial condition to plot later
 #Diffuse stuff
 
-for t in range(1,200): #t is time step
+for t in range(1,1000): #t is time step
     
     for i in range(1,N-1): #i is index on x
         
         conc1[i] = conc[i] + (conc[i-1] - 2*conc[i] + conc[i+1])*k
 
-    conc = conc1[:]
+    conc = np.copy(conc1[:]) #copy calculated concentration into your folder
+    
 
-
-
-plt.figure(2)
-plt.plot(x,conc)
+    #extract plots for 5 different times
+    
+    if t == 200:
         
+        c1 = np.copy(conc[:])
         
+    elif t == 450:
+        
+        c2 = np.copy(conc[:])
+        
+    elif t == 600:
+        
+        c3 = np.copy(conc[:])
+        
+    elif t == 800:
+        
+        c4 = np.copy(conc[:])
+        
+    elif t == 1000:
+        
+        c5 = np.copy(conc[:])
+
+plt.figure(1)
+
+plt.plot(x,c0,label= 't = 0' )
+plt.plot(x,c1,label= 't = ' + str(200/dt) )
+plt.plot(x,c2, label= 't = ' + str(450/dt))
+plt.plot(x,c3, label= 't = ' + str(600/dt))
+plt.plot(x,c4, label= 't = ' + str(800/dt))
+plt.plot(x,c5, label= 't = ' + str(1000/dt))
+plt.legend()
         
 
 
